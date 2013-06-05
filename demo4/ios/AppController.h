4da3a0ae1364791a0ee0e385221cc9b8cbbad642
le duy vu <le.duy.vu@framgia.com>
2013-06-05 10:43:30 +0700

    Initial Commit

---
+++ demo4/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  demo4AppController.h
+//  demo4
+//
+//  Created by macbook_006 on 13/06/05.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ demo4/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  demo4AppController.h
+//  demo4
+//
+//  Created by macbook_006 on 13/06/05.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
